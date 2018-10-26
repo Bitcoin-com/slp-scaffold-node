b34247a8c67888b9cf81039d5bf1d5a4fbd6811e
Gabriel Cardona <gabriel@bitcoin.com>
2018-10-26 12:04:19 +0200

    List all SLP tokens.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-let Wormhole = require('wormholecash/lib/Wormhole').default;
-let wormhole = new Wormhole();
+let SLPSDK = require("slp-sdk/lib/SLP").default;
+let SLP = new SLPSDK();
+(async () => {
+  try {
+    let list = await SLP.list();
+    console.log(list);
+  } catch (error) {
+    console.error(error);
+  }
+})();
